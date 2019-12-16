acafb1655ad04485be5851425d4296bf0c0f0ca6
shermet5 <nazar_L2013@mail.ru>
2019-12-16 11:19:41 +0000

    js

--- js/main.js
+++ js/main.js
@@ -16,3 +16,7 @@ $(function(){
     });
 
 });
+
+const clock = document.querySelector('.clock');
+const updateTime = () => clock.innerHTML = new Date().toLocaleTimeString();
+setInterval(updateTime, 1000);
